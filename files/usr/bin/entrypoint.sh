dd3d128f7e307e2c55ef978c0594b4e7e7372717
David Zager <david.j.zager@gmail.com>
2018-07-02 14:45:10 -0400

    Update entrypoint for ansible-runner

--- files/usr/bin/entrypoint.sh
+++ files/usr/bin/entrypoint.sh
@@ -18,48 +18,69 @@ fi
 # https://github.com/openshift/source-to-image/issues/475
 #
 if [[ $@ == *"s2i/assemble"* ]]; then
-  echo "---> Performing S2I build... Skipping server startup"
-  exec "$@"
-  exit $?
+    echo "---> Performing S2I build... Skipping server startup"
+    exec "$@"
+    exit $?
+fi
+
+if ! whoami &> /dev/null; then
+    if [ -w /etc/passwd ]; then
+        echo "${USER_NAME:-apb}:x:$(id -u):0:${USER_NAME:-apb} user:${HOME}:/sbin/nologin" >> /etc/passwd
+    fi
 fi
 
 ACTION=$1
 shift
-playbooks=/opt/apb/actions
+EXTRA_ARGS="${@}"
+PLAYBOOKS="/opt/apb/project"
+PASSWORDS="/opt/apb/env/passwords"
+CMDLINE="/opt/apb/env/cmdline"
 CREDS="/var/tmp/bind-creds"
 TEST_RESULT="/var/tmp/test-result"
+SECRETS_DIR="/etc/apb-secrets"
+ROLE_NAME=$(echo $2 | jq -r .role_name 2>/dev/null || echo "null")
+ROLE_NAMESPACE=$(echo $2 | jq -r .role_namespace 2>/dev/null || echo "null")
 
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-apb}:x:$(id -u):0:${USER_NAME:-apb} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
-
-SECRETS_DIR=/etc/apb-secrets
+# Handle mounted secrets
 mounted_secrets=$(ls $SECRETS_DIR)
-
-extra_args=""
 if [[ ! -z "$mounted_secrets" ]] ; then
 
-    echo '---' > /tmp/secrets
-
+    echo '---' > $PASSWORDS
     for key in ${mounted_secrets} ; do
       for file in $(ls ${SECRETS_DIR}/${key}/..data); do
-        echo "$file: $(cat ${SECRETS_DIR}/${key}/..data/${file})" >> /tmp/secrets
+        echo "$file: $(cat ${SECRETS_DIR}/${key}/..data/${file})" >> $PASSWORDS
       done
     done
-    extra_args='--extra-vars no_log=true --extra-vars @/tmp/secrets'
+    EXTRA_ARGS="${EXTRA_ARGS} --extra-vars no_log=true"
+fi
+echo "${EXTRA_ARGS}" > $CMDLINE
+
+# Install role from galaxy
+# Used when apb-base is the runner image for the ansible-galaxy adapter
+if [[ $ROLE_NAME != "null" ]] && [[ $ROLE_NAMESPACE != "null" ]]; then
+    ansible-galaxy install -s https://galaxy-qa.ansible.com $ROLE_NAMESPACE.$ROLE_NAME -p /opt/ansible/roles
+    mv /opt/ansible/roles/$ROLE_NAMESPACE.$ROLE_NAME /opt/ansible/roles/$ROLE_NAME
+    mv /opt/ansible/roles/$ROLE_NAME/playbooks $PLAYBOOKS
 fi
 
-if [[ -e "$playbooks/$ACTION.yaml" ]]; then
-  ANSIBLE_ROLES_PATH=/etc/ansible/roles:/opt/ansible/roles ansible-playbook -i /etc/ansible/hosts $playbooks/$ACTION.yaml "${@}" ${extra_args}
-elif [[ -e "$playbooks/$ACTION.yml" ]]; then
-  ANSIBLE_ROLES_PATH=/etc/ansible/roles:/opt/ansible/roles ansible-playbook -i /etc/ansible/hosts $playbooks/$ACTION.yml  "${@}" ${extra_args}
+# Move the playbooks if necessary
+if [[ ! -d "/opt/apb/project" ]]; then
+    echo "DEPRECATED: APB playbooks should be stored at /opt/apb/project"
+    mv /opt/apb/actions $PLAYBOOKS
+fi
+
+# Determine the playbook to be executed
+if [[ -e "$PLAYBOOKS/$ACTION.yaml" ]]; then
+    PLAYBOOK="$ACTION.yaml"
+elif [[ -e "$PLAYBOOKS/$ACTION.yml" ]]; then
+    PLAYBOOK="$ACTION.yml"
 else
-  echo "'$ACTION' NOT IMPLEMENTED" # TODO
+  echo "'$ACTION' NOT IMPLEMENTED"
   exit 8 # action not found
 fi
 
+# Invoke ansible-runner
+ansible-runner run --playbook $PLAYBOOK /opt/apb
 EXIT_CODE=$?
 
 set +e
@@ -67,7 +88,7 @@ rm -f /tmp/secrets
 set -e
 
 if [ -f $TEST_RESULT ]; then
-   test-retrieval-init
+    test-retrieval-init
 fi
 
 exit $EXIT_CODE
